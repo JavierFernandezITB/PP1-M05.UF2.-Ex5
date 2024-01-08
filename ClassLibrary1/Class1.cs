a30e10a8020d7501606713d5e7b9ec1d06a4adbe
JavierFernandezITB <javier.fernandez.7e7@itb.cat>
2024-01-08 13:39:58 +0100

    First commit

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace UtilsLib
+{
+    public class Utils
+    {
+        public bool IsOddNumber(int value)
+        { 
+            return (value % 2 != 0);
+        }
+
+        public int GetRandomNumber(int minRange, int maxRange)
+        { 
+            return new Random().Next(minRange, maxRange+1);
+        }
+
+        public int CountVowels(string text)
+        {
+            int count = 0;
+            string vowels = "aeiou";
+            foreach (char c in text)
+            {
+                if (vowels.Contains(Convert.ToString(c)))
+                    count++;
+            }
+            return count;
+        }
+
+        public int Pow(int number, int power)
+        {
+            int result = 1;
+            for (int i = 1; i <= power; ++i)
+            {
+                result *= number;
+            }
+            return result;
+        }
+
+        public void PressAnyKey()
+        {
+            const string Message = "Presione cualquier tecla para continuar.";
+            Console.WriteLine(Message);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using UtilsLib;
+
+namespace myNamespace
+{
+    public class myClass
+    {
+        public static void Main()
+        {
+            Utils utils = new Utils();
+
+            const string inputOption = "Que deseas hacer?";
+            const string optionsMenu = "\t1. Validar si un número es impar.\n" +
+                "\t2. Calcular la potencia de un número.\n" +
+                "\t3. Mostrar un valor aleatorio.\n" +
+                "\t4. Contar las vocales de un texto\n" +
+                "\t5. Salir";
+
+            bool mainMenuLoop = true;
+
+            while (mainMenuLoop)
+            {
+                Console.Clear();
+                Console.WriteLine(inputOption);
+                Console.WriteLine(optionsMenu);
+                switch (Convert.ToInt32(Console.ReadLine())) 
+                {
+                    case 1:
+                        ValidateNum(utils);
+                        break;
+                    
+                    case 5:
+                        mainMenuLoop = false;
+                        break;
+                }
+                utils.PressAnyKey();
+            }
+        }
+
+        public static void ValidateNum(Utils utils)
+        {
+            const string isOdd = "El valor introducido es impar.";
+            const string isNotOdd = "El valor introducido NO es impar.";
+            const string askForValue = "Introduce un valor: ";
+
+            Console.WriteLine(askForValue);
+            Console.WriteLine(utils.IsOddNumber(Convert.ToInt32(Console.ReadLine())) ? isOdd : isNotOdd);
+        }
+
+        
+    }
+}
